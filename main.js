42fb5762e9fd6b09deeec6222a2f8c6c7867bfa7
Zenpoxa <zenpoxa@gmail.com>
2024-02-06 17:47:50 +0100

    Site entièrement fonctionnel pour le format desktop

--- main.js
+++ main.js
@@ -9,24 +9,66 @@ var dico = {
 /****************************
  * JEU
  ***************************/
-var idxMotAuPif;
+var idxMotAuPif = -1;
 var continuer;
 var listeMots = Object.keys(dico);
+var bouton = document.querySelector("details+button");
+var divWipe = document.querySelector("#word-to-define+div");
 
 function chooseAword () {
     continuer = false;
 
-    idxMotAuPif = randomIntFromInterval(0, listeMots.length-1);
+    idxMotAuPif = randomIntFromInterval(0, listeMots.length-1, idxMotAuPif);
     document.querySelector("#word-to-define").innerHTML = listeMots[idxMotAuPif];
     document.querySelector("#word-definition").innerHTML = dico[listeMots[idxMotAuPif]];
-    document.querySelector("div>details").open = false;
     document.querySelector("textarea").value = "";
 }
 chooseAword();
 
+const transitionEndEventName = getTransitionEndEventName();
+
+function normalWipe() {
+    divWipe.classList.add("active");
+    document.querySelector("div>details").open = false;
+    divWipe.addEventListener(transitionEndEventName, reverseWipe);
+}
+
+function reverseWipe() {
+    divWipe.removeEventListener(transitionEndEventName, reverseWipe);
+    chooseAword();
+    divWipe.classList.remove("active");
+}
+
+bouton.addEventListener("click", normalWipe);
+
+
 /****************************
  * BOITE A OUTILS
  ***************************/
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+
+    // min et max inclus
+function randomIntFromInterval(min, max, former) {
+    let nb = Math.floor(Math.random() * (max - min + 1) + min)
+    if (nb == former) {
+        return (randomIntFromInterval(min, max, former))
+    }
+    else {
+        return nb;
+    }
 }
+
+    // obtenir la propriété de fin d'événement selon le moteur
+function getTransitionEndEventName() {
+    var transitions = {
+        "transition"      : "transitionend",
+        "OTransition"     : "oTransitionEnd",
+        "MozTransition"   : "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+     }
+    let bodyStyle = document.body.style;
+    for(let transition in transitions) {
+        if(bodyStyle[transition] != undefined) {
+            return transitions[transition];
+        } 
+    }
+  }
